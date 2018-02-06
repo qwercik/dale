76d84bc55b9262e9dbfb6654b173e43b11fc0a99
Eryk Andrzejewski <erykandrzejewski2000@gmail.com>
2018-02-06 22:27:44 +0100

    Set colors as consts. Clean up in Display::set_pixel() method

--- src/vm/display.rs
+++ src/vm/display.rs
@@ -5,6 +5,9 @@ use std::vec::Vec;
 pub const RESOLUTION: (usize, usize) = (64, 32);
 pub const PIXEL_SIZE: f32 = 20.;
 
+const FOREGROUND_COLOR: &Color = &Color::WHITE;
+const BACKGROUND_COLOR: &Color = &Color::BLACK;
+
 #[derive(Clone)]
 pub struct Pixel<'a> {
     state: bool,
@@ -14,7 +17,7 @@ pub struct Pixel<'a> {
 impl<'a> Pixel<'a> {
     pub fn new(x: usize, y: usize) -> Self {
         let mut graphics = RectangleShape::with_size((PIXEL_SIZE, PIXEL_SIZE).into());
-        graphics.set_fill_color(&Color::BLACK);
+        graphics.set_fill_color(BACKGROUND_COLOR);
         let position: Vector2<f32> = (
             x as f32 * PIXEL_SIZE,
             y as f32 * PIXEL_SIZE,
@@ -51,11 +54,13 @@ impl<'a> Display<'a> {
     pub fn set_pixel(&mut self, x: usize, y: usize, value: bool) -> bool {
         let set: bool = self.buffer[y][x].state == value;
         self.buffer[y][x].state = value;
-        if value {
-            self.buffer[y][x].graphics.set_fill_color(&Color::WHITE); 
-        } else {
-            self.buffer[y][x].graphics.set_fill_color(&Color::BLACK); 
-        }
+
+        let pixel_color = match value {
+            false => BACKGROUND_COLOR,
+            true => FOREGROUND_COLOR,
+        };
+
+        self.buffer[y][x].graphics.set_fill_color(pixel_color); 
 
         set
     }
